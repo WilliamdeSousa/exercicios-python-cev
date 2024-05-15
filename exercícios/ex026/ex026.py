4223be46e41bb2f5c31b830e6f4823b4d69bbb50
William de Sousa Costa <37248765+WilliamdeSousa@users.noreply.github.com>
2024-05-15 09:43:23 -0300

    ex027 mudado de lugar

--- exercícios/ex026/ex026.py
+++
@@ -1,7 +0,0 @@
-"""Exercício Python 027: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o
-último nome separadamente.
-
-Ex: Ana Maria de Souza (primeiro = Ana; último = Souza."""
-nome = str(input('Olá, qual o seu nome completo? ')).title().split()
-primeiroNome, ultimoNome = nome[0], nome[-1]
-print(f'Seja bem vindo, seu primeiro nome é {primeiroNome} e seu último nome é {ultimoNome}!')
